49d06c3f2ba49814ae2ac406e071c7e97bd021ac
linayaroslavna <lina.yaroslavna@gmail.com>
2019-07-25 17:46:38 +0300

    gulp-project

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+function User(firstName, secondName, age, email){
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.age = age;
+    this.email = email;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,57 @@ const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const image = require('gulp-image');
+const browsersync = require('browser-sync').create();
 
 
-// function task(done){
-//     console.log('gulp works');
-//     done();
 
-// }
-// exports.default = task;
+
+const paths = {
+    html: {
+       src: 'app/**/*.html',
+       dest: 'build/'
+    },
+    styles: {
+  
+        src: 'app/styles/**/*.scss',
+        dest: 'build/css'
+    },
+    scripts: {
+        src: 'app/js/**/*.js',
+        dest: 'build/scripts'
+    },
+    images: {
+        src: 'app/images/*.*',
+        dest: 'build/images'
+    }
+}
+
+function browserSync(done){
+    browsersync.init({
+        server:{
+            baseDir: './build'
+        },
+        port: 3000
+    });
+    done();
+}
+
+function browserSyncReload(done){
+    browsersync.reload();
+    done();
+}
+
+function html(){
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.src))
+        .pipe(browsersync.stream())
+}
 
 function styles(){
-    return gulp.src('app/styles/**/*.scss')
+    return gulp.src(paths.styles.src)
         .pipe(sass().on('error', sass.logError))
         .pipe(cssnano())
         .pipe(autoprefixer())
@@ -22,23 +62,35 @@ function styles(){
                 suffix: '.min'
             }
         ))
-        .pipe(gulp.dest('build/css'))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browsersync.stream())
 }
 
-function html(){
-    return gulp.src('app/**/*.html')
-        .pipe(gulp.dest('build/'))
+function scripts(){
+    return gulp.src(paths.scripts.src)
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browsersync.stream())
+}
+
+function images(){
+    return gulp.src(paths.images.src)
+        .pipe(image())
+        .pipe(gulp.dest(paths.images.dest))
+        .pipe(browsersync.stream())
 }
 
 function watch(){
-    gulp.watch('app/styles/**/*.scss', styles);
-    gulp.watch('app/**/*.html', html);
-    
+    gulp.watch(paths.html.src, html);
+    gulp.watch(paths.styles.src, styles);  
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.images.src, images);
+    gulp.watch('./app/*.html', gulp.series(browserSyncReload));
 }
 
-const build = gulp.parallel(styles,html);
+const build = gulp.parallel(html,styles,scripts, images);
 
 gulp.task('build', build);
 
-gulp.task('default', gulp.parallel(watch,build))
-
+gulp.task('default', gulp.parallel(watch,build,browserSync))
